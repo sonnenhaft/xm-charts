e603105117d4f6aa75495e13f003a557c61b4d7e
Vladimir Shein <vladimir.sonnenhaft@gmail.com>
2017-04-05 21:20:31 +0200

    added deploy bitballoon script

---
+++ bitballoon.js
@@ -0,0 +1,32 @@
+const {Observable} = require('rxjs/Rx');
+const bitballoon = require('bitballoon');
+
+const TRIES_NUMBER = 10;
+const DEPLOY_OPTIONS = {
+  access_token: '91bb7995f32826552ee157bc2ab888286c42094ca6bf1bffa430fcd5fcef8ff1',
+  site_id: 'xm-charts.bitballoon.com',
+  dir: './dist'
+};
+
+Observable.create(observer => {
+  bitballoon.deploy(DEPLOY_OPTIONS, error => {
+    error ? observer.error(error) : observer.next()
+  });
+}).retryWhen(errors => errors.scan((errorCount, err) => {
+  if (errorCount >= TRIES_NUMBER || typeof err === 'string' && err.includes('No such dir')) {
+    throw err;
+  }
+  const nextTryNumber = errorCount + 1;
+  console.warn(`Deploy failed because ${err}. Making new try (#${nextTryNumber}) in a second.`);
+  return nextTryNumber;
+}, 1).delay(300)).subscribe( // TODO: remove first delay
+  next =>console.log('Deploy successfully completed!'),
+  error => console.error(`
+    ${error}
+    ------
+    Deploy failed, not trying again. 
+    1. Check if files in "dist" folder exists and "index.html" exists.
+    2. Check if you have proper existing "access_token".
+    3. Check if you have proper existing "site_id".
+    4. Check if http://bitballoon.com is live and running.`)
+)
--- src/components/Demo.js
+++ src/components/Demo.js
@@ -2,6 +2,10 @@ import React from 'react'
 import TimelineChartWrapper from './TimelineChartWrapper/TimelineChartWrapper'
 import generateDemoData from './TimelineChartWrapper/generateDemoData'
 
+import packageJson from '../../package.json'
+
+
+
 const Demo = () => (
   <div>
     <br />
@@ -10,10 +14,23 @@ const Demo = () => (
     <br />
     <br />
     <TimelineChartWrapper data={generateDemoData()} />
+
+    <div className="version">{packageJson.name} v{packageJson.version}</div>
     <style>{`
       body {
         margin: 0;
-      }`}
+      }
+      .version {
+        display: block;
+        position: fixed;
+        font-size: 25px;
+        font-family: sans-serif;
+        right: 5px;
+        bottom: 5px;
+        background: white;
+        color: black;
+      }
+      `}
     </style>
   </div>
 )
