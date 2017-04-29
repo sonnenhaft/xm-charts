e3c98c40e80c39651ca689b283532a8460ef093b
Vladimir Shein <vladimir.sonnenhaft@gmail.com>
2017-04-29 03:40:32 +0200

    made legs clickable

--- src/components/Timeline/TimelineChart/TimelineChartUtils.js
+++ src/components/Timeline/TimelineChart/TimelineChartUtils.js
@@ -65,13 +65,13 @@ export const renderCircles = ({ g, data, x, height, duration, bulkLines, firstIn
   enteredSelection.exit().remove()
 
   const mergedSelection = enteredSelection.enter().append('g')
-    .attr('class', 'bulkBlock').html(({ value }) => `<g transform="translate(1, 0)">
-    <rect fill="#EB001E"  pointer-events="none" class="white-shadow-rect"
+    .attr('class', 'bulkBlock').html(({ value }) => `<g transform="translate(1, 0)" cursor="pointer">
+    <rect fill="#EB001E" class="white-shadow-rect"
         transform="translate(${-(radius + 1)})"    width="${(radius + 1) * 2}"></rect>
      <circle class="circleWrapper" fill="#252525"  stroke-opacity="${value ? 0.3 : 1}"
         stroke-width="${value ? 4 : 2}"  r="${radius}" stroke="#EB001E"></circle>
      <circle class="${styles['red-bulk-circle']}" r="${value ? radius : radius / 2}" fill="#EB001E"></circle>
-     <rect class="${styles['red-bulk-line']}" width="${radius / 4}" pointer-events="none"
+     <rect class="${styles['red-bulk-line']}" width="${radius / 4}"
         transform="translate(${-radius / 8}, ${radius - 1})" ></rect>
      <text transform="translate(0, 3.5)" class="${styles['circle-text']}">${value || ''}</text>
   </g>`)
