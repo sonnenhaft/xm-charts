bed99aec00a8511384fef214e630b2bce2d4af90
Dani Kenan <danikenan@gmail.com>
2017-05-01 09:00:44 +0300

    fixed simulation chart start time + added json files

--- src/charts/components/SimulationChart.js
+++ src/charts/components/SimulationChart.js
@@ -1,22 +1,37 @@
 import React from 'react'
 import Timeline from './Timeline'
 import NetworkGrid from './NetworkGrid'
-import {withState, compose, withPropsOnChange} from 'recompose'
+import {compose, withPropsOnChange} from 'recompose'
 
 import './SimulationChart.scss'
 import eventsAndNodesAdapter from '../utils/eventsAndNodesAdapter'
 
-const SimulationChart = ({events = [], nodes = [], className = '', currentTime, onCurrentTimeChanged}) => (
-  <div className={className} styleName="root">
-    <NetworkGrid styleName="network" {...{events, nodes, currentTime}}/>
-    <Timeline styleName="timeline" {...{events, nodes, currentTime, onCurrentTimeChanged}} />
-  </div>
-)
+class SimulationChart extends React.Component {
 
+  state = {currentTime: 0}
+
+  onCurrentTimeChanged = currentTime => this.setState({currentTime})
+
+  componentWillReceiveProps({nodes, events}){
+    if(nodes !== this.props.nodes || events !== this.props.events){
+      this.setState({currentTime: events.length && events[events.length - 1].date})
+    }
+  }
+
+  render(){
+    const {props: {events, nodes, className}, state: {currentTime}, onCurrentTimeChanged } = this
+    return (
+      <div className={className} styleName="root">
+        <NetworkGrid styleName="network" {...{events, nodes, currentTime}}/>
+        <Timeline styleName="timeline" {...{events, nodes, currentTime, onCurrentTimeChanged}} />
+      </div>
+    )
+  }
+}
 
 const enhance = compose(
   withPropsOnChange(['nodes', 'events'], ({nodes, events}) => eventsAndNodesAdapter({events, nodes})),
-  withState('currentTime', 'onCurrentTimeChanged', ({events}) => events.length && events[events.length - 1].date)
 )
 
 export default enhance(SimulationChart)
+
