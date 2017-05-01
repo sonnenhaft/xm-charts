fe2d4781a791095ad39e7a52095a04585c2cf60e
Dani Kenan <danikenan@gmail.com>
2017-05-01 04:12:11 +0300

    fixed flixering and changing of layout during play

--- src/charts/components/SimulationChart.js
+++ src/charts/components/SimulationChart.js
@@ -1,23 +1,22 @@
 import React from 'react'
 import Timeline from './Timeline'
 import NetworkGrid from './NetworkGrid'
-import { withState } from 'recompose'
+import {withState, compose, withPropsOnChange} from 'recompose'
 
 import './SimulationChart.scss'
 import eventsAndNodesAdapter from '../utils/eventsAndNodesAdapter'
 
-const SimulationChart = ({ events: _events = [], nodes: _nodes = [], className = '', currentTime, onCurrentTimeChanged }) => {
-  const { events, nodes } = eventsAndNodesAdapter({events: _events, nodes: _nodes})
-  return (
-    <div className={className} styleName="root">
-      <NetworkGrid styleName="network" {...{ events, nodes, currentTime }}/>
-      <Timeline styleName="timeline" {...{ events, nodes, currentTime, onCurrentTimeChanged }} />
-    </div>
-  )
-}
+const SimulationChart = ({events = [], nodes = [], className = '', currentTime, onCurrentTimeChanged}) => (
+  <div className={className} styleName="root">
+    <NetworkGrid styleName="network" {...{events, nodes, currentTime}}/>
+    <Timeline styleName="timeline" {...{events, nodes, currentTime, onCurrentTimeChanged}} />
+  </div>
+)
 
-const enhance = withState('currentTime', 'onCurrentTimeChanged', ({ events }) => {
-  return events.length && new Date(events[events.length - 1].timestamp).getTime()
-})
+
+const enhance = compose(
+  withPropsOnChange(['nodes', 'events'], ({nodes, events}) => eventsAndNodesAdapter({events, nodes})),
+  withState('currentTime', 'onCurrentTimeChanged', ({events}) => events.length && new Date(events[events.length - 1].timestamp).getTime())
+)
 
 export default enhance(SimulationChart)
