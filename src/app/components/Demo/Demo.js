a4704a18a7e538d2b5aa0c9279b366718cf16977
Georgii Kats <kats.georgii@gmail.com>
2017-05-01 11:43:03 +0200

    Add select.

--- src/app/components/Demo/Demo.js
+++ src/app/components/Demo/Demo.js
@@ -2,19 +2,52 @@ import React, {Component} from 'react'
 import SimulationChart from 'charts/components/SimulationChart'
 import './Demo.scss'
 
-import nodes from './nodes1.json'
-import events from './events1.json'
-
-
 const version = `${process.env.npm_package_name} v${process.env.npm_package_version}`
+const options = [
+  {value: 1, title: 1},
+  {value: 2, title: 2},
+  {value: 3, title: 3},
+]
 
-const Bar = ({className}) => (<div className={className}>Toolbar here ({version})</div>)
+const Bar = ({className, value, ...rest}) => (
+  <div className={className}>
+    <select value={value} {...rest}>
+      {options.map(({value, title}) => 
+        <option key={value} value={value}>{title}</option>
+      )}
+    </select>
+    Toolbar here ({version})
+  </div>
+)
 
 class Demo extends Component {
+  state = {
+    events: [],
+    nodes: [],
+    option: options[0].value,
+  }
+
+  componentDidMount() {
+    this.onLoadData(this.state.option)
+  }
+
+  onLoadData(value) {
+    const events = require(`./events${value}.json`)
+    const nodes = require(`./nodes${value}.json`)
+
+    this.setState({option: value, events, nodes})
+  }
+
+  // onOptionChange = (value) => {
+  //   this.setState({option: value}, () => this.onLoadData(value))
+  // }
+
   render() {
+    const {option, events, nodes} = this.state
+
     return (
       <div styleName="root">
-        <Bar styleName="toolbar"/>
+        <Bar styleName="toolbar" value={option} onChange={(event) => this.onLoadData(event.target.value)} />
         <SimulationChart styleName="simulation-chart" events={events} nodes={nodes}/>
       </div>
     )
