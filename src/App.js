bb8110b220e9ae82bb2e416a9721f74880f4d272
Georgii Kats <kats.georgii@gmail.com>
2017-05-26 09:53:53 +0200

    Implement loader.

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import Router from 'react-router-dom/HashRouter'
 import { Route } from 'react-router-dom'
 import Demo from './app/components/Demo'
 import Animations from './animations/components/Sample'
+import Loader from './animations/components/Loader'
 
 const App = () => (
   <Router>
     <div>
       <Route exact path="/" component={Demo} />
       <Route exact path="/animations" component={Animations} />
+      <Route exact path="/loader" component={Loader} />
     </div>
   </Router>
 )
---
+++ src/animations/components/Loader.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import './Loader.scss'
+
+const Loader = () => (
+  <div styleName="container">
+  <div styleName='loader'>
+    <div>
+      <div>
+        <div>
+          <div>
+            <div>
+              <div />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div styleName="description">
+    Loading Something…
+  </div>
+  </div>
+)
+
+export default Loader
