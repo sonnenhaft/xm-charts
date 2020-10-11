734c551c8f9c91f479929ceb0f85282763aad869
vlad <vladimir.shein@accedo.tv>
2020-10-11 18:57:17 +0800

    Added gh-pages

--- src/charts/index.js
+++
@@ -1,3 +0,0 @@
-export SimulationChart from './components/SimulationChart'
-
-
--- src/charts/utils/EventUtils.js
+++
@@ -1,5 +0,0 @@
-export const isAssetCompromised = type => 'assetCompromised' === type
-
-export const isStaringPoint = type => 'newStartingPointNode' === type
-
-export const isSpecial = ({ type }) => isStaringPoint(type) || isAssetCompromised(type)
